be33d610b5c1e2709f51fe20015d9d85e1132447
adnba <39657038+adnba@users.noreply.github.com>
2020-12-31 09:41:44 +0100

    added fave logo

---
+++ src/components/Fave.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+class Fave extends Component {
+  render() {
+    return (
+      <div className="film-row-fave add_to_queue">
+        <p>
+          <i className="material-icons">add_to_queue</i>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Fave
